937981a698e52910c777899a4b81b51ac5ad8912
Liene <liene.krista@gmail.com>
2023-10-06 17:38:27 +0300

    Added custom launch color, removed unnecesary launch screen

--- Planet Pet Pals/Planet Pet Pals/Planet_Pet_PalsApp.swift
+++ Planet Pet Pals/Planet Pet Pals/Planet_Pet_PalsApp.swift
@@ -19,27 +19,9 @@ import SwiftUI
 
 @main
 struct Planet_Pet_PalsApp: App {
-    @StateObject private var loadingState = LoadingState()
-
     var body: some Scene {
         WindowGroup {
-            Group {
-                if loadingState.isLoading {
-                    LoadingView()
-                } else {
-                    ContentView()
-                }
-            }
-            .onAppear {
-                // Simulating a delay
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    withAnimation(.easeIn(duration: 0.3)) {
-                        loadingState.isLoading = false
-                    }
-                }
-            }
-            .transition(.opacity)
-            .animation(.easeIn(duration: 0.3), value: loadingState.isLoading)
+            ContentView()
         }
     }
 }
